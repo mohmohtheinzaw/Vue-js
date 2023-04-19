6b4183e016505e4cb9205ce67357fbd39b7d3f0b
mohmoh <mohmohtheinzaw379@gmail.com>
2023-04-19 22:10:58 +0630

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <h1>How fast can u catch me?</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <div v-if="isPlaying">
+      <BlockComponent :delay="delay" />
+  </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BlockComponent from "./components/BlockComponent.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name:'App',
+  data(){
+    return {
+      isPlaying :false,
+      delay:null,
+    }
+  },
+
+  //Register component
+  components:{
+    BlockComponent
+  },
+  methods:{
+   start(){
+    this.isPlaying= true;
+    this.delay =2000+Math.random()*5000
+   }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{success:theme =='success',danger:theme=='delete'}"> 
+        <h2>{{header}}</h2>
+        <p>{{content}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  //get data from parent component to child component
+  props:['header','content','theme'],
+  methods:{
+    closeModal(){
+      //use data emit when we need to change the data of the parent component (from child component)
+      this.$emit("close")
+    }
+  }
+}
+</script>
+
+<style>
+.backdrop{
+    position: fixed;
+    top:0;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+.modal{
+    width: 400px;
+    padding: 30px;
+    margin: 100px auto;
+    background-color: white;
+    border-radius: 10px;
+}
+.success{
+  background-color: green;
+  color: white;
+}
+.danger{
+  background-color: red;
+  color: white;
+}
+</style>
